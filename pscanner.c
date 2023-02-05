06f5ac4b7773dccf8e6f501f0daa4a5acb548ff3
lucdev <me@lucdev.net>
2023-02-05 16:33:04 -0300

    Add validations and asserts

--- pscanner.c
+++ pscanner.c
@@ -1,14 +1,19 @@
-#include <pthread.h>
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
+#include <stdio.h>
+#include <pthread.h>
 #include <unistd.h>
 #include <sys/time.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <netinet/in.h>
+#include <assert.h>
+#include <arpa/inet.h>
 
 #define NUM_PARTS 20
+
+// Todo: Make an array and store the results in it.
+// This way we can check if we already scanned a port or not.
+// int** scanned_ports;
 
 typedef struct range {
     int start;
@@ -21,52 +26,78 @@ struct scan_args {
     int timeout;
 };
 
-void *scan_port_thread(void *arg) {
-    struct scan_args *args = (struct scan_args *)arg;
+int scan_tcp_port(char *ip, int port, int timeout) {
+    assert(ip != NULL);
+    assert(port > 0 && port < 65535);
+    assert(timeout >= 0);
+
+    int result = 0;
     int sock;
     struct sockaddr_in server;
     struct timeval tv;
 
-    for (int port = args->ports->start; port < args->ports->end; port++) {
-        // Create the socket
-        sock = socket(AF_INET, SOCK_STREAM, 0);
-        if (sock == -1) {
-            perror("socket");
-            exit(1);
-        }
-
-        // Set the timeout on the socket
-        tv.tv_sec = args->timeout;
-        tv.tv_usec = 0;
-        if (setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv)) < 0) {
-            perror("setsockopt");
-            exit(1);
-        }
-        if (setsockopt(sock, SOL_SOCKET, SO_SNDTIMEO, &tv, sizeof(tv)) < 0) {
-            perror("setsockopt");
-            exit(1);
-        }
-        // Prepare the sockaddr_in structure
-        memset(&server, 0, sizeof(server));
-        server.sin_family = AF_INET;
-        server.sin_port = htons(port);
-        if (inet_pton(AF_INET, args->ip, &server.sin_addr) <= 0) {
-            perror("inet_pton");
-            exit(1);
-        }
-
-        // Connect to the server
-        if (connect(sock, (struct sockaddr *)&server, sizeof(server)) < 0) {
-            printf("%d: closed\n", port);
-        } else {
-            printf("%d: open --- FOUND OPEN PORT ---\n", port);
-        }
-
-        // Close the socket
-        close(sock);
+    // Create the socket
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == -1) {
+        perror("socket");
+        exit(1);
     }
-    
 
+    // Set the timeout on the socket
+    tv.tv_sec = timeout;
+    tv.tv_usec = 0;
+    if (setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv)) < 0) {
+        perror("setsockopt");
+        exit(1);
+    }
+    if (setsockopt(sock, SOL_SOCKET, SO_SNDTIMEO, &tv, sizeof(tv)) < 0) {
+        perror("setsockopt");
+        exit(1);
+    }
+    // Prepare the sockaddr_in structure
+    memset(&server, 0, sizeof(server));
+    server.sin_family = AF_INET;
+    server.sin_port = htons(port);
+    if (inet_pton(AF_INET, ip, &server.sin_addr) <= 0) {
+        perror("inet_pton");
+        exit(1);
+    }
+
+    // Connect to the server
+    if (connect(sock, (struct sockaddr *)&server, sizeof(server)) < 0) {
+        result = 0;
+    } else {
+        result = 1;
+    }
+
+    // Close the socket
+    close(sock);
+
+    return result;
+}
+
+void *scan_port_thread(void *arg) {
+    struct scan_args *args = (struct scan_args *)arg;
+
+    assert(args->ip != NULL);
+    assert(args->ports->start > args->ports->end);
+    assert(args->ports->start < 1 || args->ports->end > 65535);
+    assert(args->timeout >= 0);
+    assert(args->ports != NULL);
+
+    if (args->ports->start == args->ports->end) {
+        printf("Scanning port %d\n", args->ports->start);
+        if (scan_tcp_port(args->ip, args->ports->start, args->timeout)) {
+            printf("Port %d -- OPEN\n", args->ports->start);
+        }
+    } else {
+        printf("Scanning ports %d to %d\n", args->ports->start, args->ports->end);
+        for (int port = args->ports->start; port < args->ports->end; port++) {
+            if (scan_tcp_port(args->ip, port, args->timeout)) {
+                printf("Port %d -- OPEN\n", port);
+            }
+        }
+    }
     return NULL;
 }
 
@@ -90,16 +121,33 @@ struct range *divide_range(int start, int end) {
 }
 
 int main(int argc, char *argv[]) {
+    // Usage
     if (argc != 5) {
         printf("Usage: %s <ip address> <start_port> <end_port> <timeout>\n", argv[0]);
         return 1;
     }
 
+    // Validations
+    if (atoi(argv[2]) > atoi(argv[3])) {
+        fprintf(stderr, "Invalid port range\n");
+        return 1;
+    }
+
+    if (atoi(argv[2]) < 1 || atoi(argv[3]) > 65535) {
+        fprintf(stderr, "Invalid port range\n");
+        return 1;
+    }
+
+    if (atoi(argv[4]) < 0) {
+        fprintf(stderr, "Invalid timeout\n");
+        return 1;
+    }
+
+    // Get ranges
     struct range *ranges = divide_range(atoi(argv[2]), atoi(argv[3]));
 
     pthread_t threads[NUM_PARTS];
     struct scan_args args[NUM_PARTS];
-
 
     // Read the command line arguments
     for (int i = 0; i < NUM_PARTS; i++) {
