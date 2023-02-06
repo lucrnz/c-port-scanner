fffbcb1cfcda2110b75b09042dcbe0d59da53b67
lucdev <me@lucdev.net>
2023-02-05 23:16:59 -0300

    Implement array to check already scanned ports

--- pscanner.c
+++ pscanner.c
@@ -10,10 +10,11 @@
 #include <arpa/inet.h>
 
 #define NUM_PARTS 20
+#define MAX_PORTS 65535
 
-// Todo: Make an array and store the results in it.
-// This way we can check if we already scanned a port or not.
-// int** scanned_ports;
+pthread_mutex_t lock;
+
+int* scanned_ports;
 
 typedef struct range {
     int start;
@@ -28,9 +29,20 @@ struct scan_args {
 
 int scan_tcp_port(char *ip, int port, int timeout) {
     assert(ip != NULL);
-    assert(port > 0 && port < 65535);
+    assert(port > 0 && port < MAX_PORTS);
     assert(timeout >= 0);
 
+    // Check if we already scanned this port
+    pthread_mutex_lock(&lock);
+    if (scanned_ports[port] != 0) {
+        printf("Port %d already scanned\n", port);
+        int value = scanned_ports[port];
+        pthread_mutex_unlock(&lock);
+        return value - 1;
+    } else {
+        pthread_mutex_unlock(&lock);
+    }
+    
     int result = 0;
     int sock;
     struct sockaddr_in server;
@@ -73,6 +85,10 @@ int scan_tcp_port(char *ip, int port, int timeout) {
     // Close the socket
     close(sock);
 
+    pthread_mutex_lock(&lock);
+    scanned_ports[port] = result + 1;
+    pthread_mutex_unlock(&lock);
+
     return result;
 }
 
@@ -81,7 +97,7 @@ void *scan_port_thread(void *arg) {
 
     assert(args->ip != NULL);
     assert(args->ports->end >= args->ports->start);
-    assert(args->ports->start > 0 || args->ports->end <= 65535);
+    assert(args->ports->start > 0 || args->ports->end < MAX_PORTS);
     assert(args->timeout >= 0);
     assert(args->ports != NULL);
 
@@ -90,20 +106,27 @@ void *scan_port_thread(void *arg) {
         if (scan_tcp_port(args->ip, args->ports->start, args->timeout)) {
             printf("Port %d -- OPEN\n", args->ports->start);
         }
-    } else {
-        printf("Scanning ports %d to %d\n", args->ports->start, args->ports->end);
-        for (int port = args->ports->start; port < args->ports->end; port++) {
-            if (scan_tcp_port(args->ip, port, args->timeout)) {
-                printf("Port %d -- OPEN\n", port);
-            }
+        return NULL;
+    }
+    
+    printf("Scanning ports %d to %d\n", args->ports->start, args->ports->end);
+    for (int port = args->ports->start; port < args->ports->end; port++) {
+        if (scan_tcp_port(args->ip, port, args->timeout)) {
+            printf("Port %d -- OPEN\n", port);
         }
     }
+
     return NULL;
 }
 
 struct range *divide_range(int start, int end) {
     int step = (end - start + NUM_PARTS - 1) / NUM_PARTS;
     struct range *ranges = malloc(NUM_PARTS * sizeof(struct range));
+    
+    if (ranges == NULL) {
+        perror("malloc");
+        exit(1);
+    }
 
     int current_start = start;
     int current_end = start + step - 1;
@@ -127,24 +150,47 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
+    char* ip_address = argv[1];
+    int port_range_start = atoi(argv[2]);
+    int port_range_end = atoi(argv[3]);
+    int timeout = atoi(argv[4]);
+
     // Validations
-    if (atoi(argv[2]) > atoi(argv[3])) {
+    if (ip_address == NULL) {
+        fprintf(stderr, "Invalid IP address\n");
+        return 1;
+    }
+
+    if (port_range_start > port_range_end || port_range_start == port_range_end) {
         fprintf(stderr, "Invalid port range\n");
         return 1;
     }
 
-    if (atoi(argv[2]) < 1 || atoi(argv[3]) > 65535) {
+    if (port_range_start < 1 || port_range_end > MAX_PORTS) {
         fprintf(stderr, "Invalid port range\n");
         return 1;
     }
 
-    if (atoi(argv[4]) < 0) {
+    if (timeout < 0) {
         fprintf(stderr, "Invalid timeout\n");
         return 1;
     }
 
+    // Initialize the ports array
+    scanned_ports = calloc(MAX_PORTS, sizeof(int));
+    if (scanned_ports == NULL) {
+        perror("calloc");
+        exit(1);
+    }
+
+    // Initialize the mutex
+    if (pthread_mutex_init(&lock, NULL) != 0) {
+        fprintf(stderr, "Error initializing mutex\n");
+        exit(1);
+    }
+
     // Get ranges
-    struct range *ranges = divide_range(atoi(argv[2]), atoi(argv[3]));
+    struct range *ranges = divide_range(port_range_start, port_range_end);
 
     pthread_t threads[NUM_PARTS];
     struct scan_args args[NUM_PARTS];
@@ -170,6 +216,12 @@ int main(int argc, char *argv[]) {
         pthread_join(threads[i], NULL);
     }
 
+    // Destroy the mutex
+    pthread_mutex_destroy(&lock);
+
+    // Free the memory
+    free(scanned_ports);
     free(ranges);
+
     return 0;
 }
